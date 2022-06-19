caf31dc4be477990b490228de579c6fc2c51e6cd
Mink-a <rahulakhant@gmail.com>
2022-06-19 20:45:11 +0630

    add recipe details to veggie and popular

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -3,12 +3,14 @@ import Home from "./Home";
 import { Route, Routes } from "react-router-dom";
 import Crusine from "./Crusine";
 import Search from "./Search";
+import Recipe from "./Recipe";
 function Pages() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/crusine/:type" element={<Crusine />} />
       <Route path="/search/:search" element={<Search />} />
+      <Route path="/recipe/:name" element={<Recipe />} />
     </Routes>
   );
 }
